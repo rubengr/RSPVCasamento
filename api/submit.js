a03d6221c04032e3d99f9bc6c35c916fe6b26f20
Rúben Rodrigues <ruben_gr@live.com.pt>
2025-03-14 21:07:05 +0000

    fix format

--- api/submit.js
+++ api/submit.js
@@ -8,19 +8,30 @@ export default async function handler(req, res) {
         // URL do Google Apps Script
         const scriptUrl = "https://script.google.com/macros/s/AKfycbza-WzzYENogOPlfrDTYBibYTAsQME_iGU2cah2JIrdxSYy8NKMwIHYP0_W2hyzy1bn/exec";
 
+        const data = {
+            data: {
+                name: req.body.data.name,
+                email: req.body.data.email,
+                message: req.body.data.message || "",
+                adults: req.body.data.adults || "[]",
+                kids4to9: req.body.data.kids4to9 || "[]",
+                kids0to3: req.body.data.kids0to3 || "[]",
+            }
+        };
+
         // Faz a requisição POST para o Google Apps Script
         const response = await fetch(scriptUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(req.body),
+            body: JSON.stringify(data),
             redirect: 'follow', // Segue redirecionamentos
         });
 
         // Retorna a resposta do Google Apps Script
-        const data = await response.json();
-        res.status(200).json(data);
+        const result = await response.json();
+        res.status(200).json(result);
     } catch (error) {
         console.error('Erro:', error);
         res.status(500).json({ error: 'Erro ao processar a requisição' });
